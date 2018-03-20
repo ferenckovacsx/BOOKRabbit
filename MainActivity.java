e84779ee7ad4a713cd2edc6980d1673dce81a4a2
Ferenc Kovacs <ferenckovacsx@gmail.com>
2018-03-20 01:58:17 +0100

    Rabbit animation working. Initial In-App Billing implementation.

--- MainActivity.java
+++ MainActivity.java
@@ -1,28 +1,46 @@
 package com.example.rabbit;
 
 import android.animation.ValueAnimator;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.FileNotFoundException;
+import com.example.rabbit.util.IabBroadcastReceiver;
+import com.example.rabbit.util.IabHelper;
+import com.example.rabbit.util.IabResult;
+import com.example.rabbit.util.Inventory;
+import com.example.rabbit.util.Purchase;
+
 import java.io.IOException;
 
 import pl.droidsonroids.gif.GifDrawable;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "MainActivity";
+    static final String SKU_SUBSCRIBE = "carrot_subscription";
+    static final String SKU_CARROT = "carrot_single";
+
     ImageView rabbitImageView, grassImageView1, grassImageView2;
     Button runButton;
     GifDrawable gifDrawable;
-    Boolean isRunning = false;
-
+    boolean isRunning = false;
     ValueAnimator animator;
+
+    int carrotCount;
+
+    String subscriptionSku;
+    String carrotSku;
+    boolean isSubscribed;
+    boolean isAutoRenewed = false;
+    IabHelper inAppBillingHelper;
+    IabBroadcastReceiver inAppBillingBroadcastReceiver;
+    String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn3U1RFdCwsSru9+ASzi/AM7Zxrha34KsJekqLPIAL/JdZR9nS1W4jfIMnZ5G16MEqKW53onO7NF85kk5FBp0ypfiS/rkU58QHs9PlBMTZm6kcV158AFQsc6W/5KC3QZ9t1FMRL/bCQCALM5qed+nwXtPRQN5jmwjTcVMvzcex9fFVDU74/L40FImfDNmh8lWUMtLE57O/ZNgWfs1QywQZC2HacUcE5U6RLUbpVA+hLgpmXG0ZzNLdPs72rAF+SYN9IaYmhVmmqTOc81ir7+o/k4SVt7Uo7WYThii7ZaiLb/foXNFHywBVVXuB0OCKA45P1GZgAJ6ZczRvZ03lNNCfwIDAQAB";
 
 
     @Override
@@ -37,13 +55,25 @@ public class MainActivity extends AppCompatActivity {
 
         animator = ValueAnimator.ofFloat(1.0f, 0.0f);
 
+        inAppBillingHelper = new IabHelper(this, base64EncodedPublicKey);
+        inAppBillingHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
 
-//        Animation marquee = AnimationUtils.loadAnimation(this, R.anim.animation_grass);
-//        grassImageView1.setAnimation(marquee);
+                if (!result.isSuccess()) {
+                    Log.e(TAG, "In-App Billing setup was unsuccessful: " + result);
+                    return;
+                }
+                if (inAppBillingHelper == null) return;
 
-
-
-
+                //Get inventory
+                try {
+                    inAppBillingHelper.queryInventoryAsync(mGotInventoryListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    Log.e(TAG, "Error querying inventory. Another async operation in progress.");
+                }
+            }
+        });
 
 
         try {
@@ -62,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
 
     void toggleRunning() {
@@ -94,4 +123,68 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+
+            // Have we been disposed of in the meantime? If so, quit.
+            if (inAppBillingHelper == null) return;
+
+            // Is it a failure?
+            if (result.isFailure()) {
+                Log.e(TAG, "Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+
+
+            // First find out which subscription is auto renewing
+            Purchase gasMonthly = inventory.getPurchase(SKU_SUBSCRIBE);
+            if (gasMonthly != null && gasMonthly.isAutoRenewing()) {
+                subscriptionSku = SKU_SUBSCRIBE;
+                isAutoRenewed = true;
+            } else {
+                subscriptionSku = "";
+                isAutoRenewed = false;
+            }
+
+            isSubscribed = (gasMonthly != null);
+            Log.d(TAG, "User " + (isSubscribed ? "HAS" : "DOES NOT HAVE") + " infinite gas subscription.");
+
+//            if (isSubscribed) mTank = TANK_MAX;
+
+            // Check for gas delivery -- if we own gas, we should fill up the tank immediately
+            Purchase gasPurchase = inventory.getPurchase(SKU_CARROT);
+            if (gasPurchase != null) {
+
+                carrotCount += 1;
+
+//                try {
+//                    carrotCount += 1;
+////                    inAppBillingHelper.consumeAsync(inventory.getPurchase(SKU_GAS), mConsumeFinishedListener);
+//                } catch (IabHelper.IabAsyncInProgressException e) {
+//                    Log.e(TAG, "Error consuming gas. Another async operation in progress.");
+//                }
+                return;
+            }
+
+//            updateUi();
+//            setWaitScreen(false);
+        }
+    };
+
+//    @Override
+//    public void receivedBroadcast() {
+//        // Received a broadcast notification that the inventory of items has changed
+//        Log.d(TAG, "Received broadcast notification. Querying inventory.");
+//        try {
+//            inAppBillingHelper.queryInventoryAsync(mGotInventoryListener);
+//        } catch (IabHelper.IabAsyncInProgressException e) {
+//            complain("Error querying inventory. Another async operation in progress.");
+//        }
+//    }
+
+
 }
